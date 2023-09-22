45f196c7d1c1463d536c7692f3296107cf8e849e
MathieuBretaud <mathieu.bretaud@gmail.com>
2023-09-22 09:36:04 +0200

    layout done

---
+++ pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import { s } from "./Home.style";
+export function Home() {
+  return (
+    <>
+      <View style={s.meteo_basic}>
+        <Text>Hello</Text>
+      </View>
+      <View style={s.searchbar_container}></View>
+      <View style={s.meteo_advanced}></View>
+    </>
+  );
+}
