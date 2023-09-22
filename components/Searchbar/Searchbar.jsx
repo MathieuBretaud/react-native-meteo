81c9e6fa29e4784e2cac28f8f5085427a8582980
MathieuBretaud <mathieu.bretaud@gmail.com>
2023-09-22 16:47:30 +0200

    add searchbar

---
+++ components/Searchbar/Searchbar.jsx
@@ -0,0 +1,11 @@
+import { TextInput } from "react-native";
+import { s } from "./Searchbar.style";
+export function Searchbar({ onSubmit }) {
+  return (
+    <TextInput
+      onSubmitEditing={onSubmit}
+      style={s.input}
+      placeholder="Chercher une ville... Ex: Paris"
+    />
+  );
+}
